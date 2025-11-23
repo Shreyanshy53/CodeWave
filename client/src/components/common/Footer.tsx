c979044934ea895afd4b88e7a73d2e4857bf2ee4
Shreyansh Yadav <shre3250@gmail.com>
2025-11-23 10:07:54 +0530

    Full project upload

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -4,10 +4,10 @@ function Footer() {
             <span>
                 Build with ❤️ by{" "}
                 <a
-                    href="https://github.com/sahilatahar"
+                    href="https://github.com/Shreyanshy53"
                     className="text-primary underline underline-offset-1"
                 >
-                    sahilatahar
+                    Shreyansh Yadav
                 </a>
             </span>
         </footer>
